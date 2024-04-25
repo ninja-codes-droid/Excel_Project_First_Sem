8d0142bfdd115df2fa6aa2bfdb742480d9d0ba3b
Ninja <codehub5565@gmail.com>
2024-04-25 23:54:28 +0530

    24/04/2024

--- jdbc-userlogin/src/main/java/repository/storageRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-public abstract class storageRepository<T> {
-	public abstract boolean store(T item);
-	public abstract T retrive(T item);
-	public abstract boolean remove(T item);
-}
